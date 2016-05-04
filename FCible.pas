71d1721120bcd131f569ea3b4344ee08da762975
darkgallium <developer@le-minoux.fr>
2016-05-04 17:00:46 +0200

    Creating the base machanism

--- FCible.pas
+++ FCible.pas
@@ -3,7 +3,7 @@ unit FCible;
 interface
 
 uses
-  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms,
   Dialogs, ExtCtrls,
   UFonctionnelle;
 
@@ -29,6 +29,7 @@ implementation
 procedure TForm3.FormCreate(Sender: TObject);
 begin
     genererEcran(Form3.Image1);
+    afficherSolutions(Form3.Image1,1);
 end;
 
 procedure TForm3.Image1Click(Sender: TObject);
@@ -37,10 +38,10 @@ var
   posLig,posCol: integer;
 
 begin
-      posCol:=0;posLig:=0;
+      (*posCol:=0;posLig:=0;
       pointeurSouris := ScreenToClient(Mouse.CursorPos); // Obtention de la position de la souris dans la fenêtre Form1
       ObtenirCoordTableau(pointeurSouris.x,pointeurSouris.y,posCol,posLig);
-      verifierCase(0,posCol,posLig);
+      verifierCase(0,posCol,posLig); *)
 end;
 
 end.
